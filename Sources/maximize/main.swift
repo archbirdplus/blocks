86f096290dc35fbe7e1adf7ae5028aaee4bc16e7
Archbirdplus <archbirdplus@gmail.com>
2024-02-28 15:26:50 -0800

    Arg commandline arg to Maximize to switch level

--- Sources/maximize/main.swift
+++ Sources/maximize/main.swift
@@ -1,5 +1,18 @@
 import blocks
 
-let searcher = Searcher(Skill.skills, level: .diamond)
+let level: Routine.Level
+if CommandLine.arguments.count < 2 {
+    level = .diamond
+} else {
+    let arg = CommandLine.arguments[1]
+    let l = Routine.Level.named(arg)
+    guard let l = l else {
+        print("Unrecognized blocks level: \"\(arg)\"")
+        exit(1)
+    }
+    level = l
+}
+print("Maximizing difficulty for \(level.name).")
+let searcher = Searcher(Skill.skills, level: level)
 searcher.search()
 
